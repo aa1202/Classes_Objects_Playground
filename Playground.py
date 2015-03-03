562b47575fc934fea9107d21c2d1f16790524434
Andreas Amundsen <andreas.amundsen123@gmail.com>
2015-03-03 11:10:19 +0100

    Calculator Script

--- Playground.py
+++ Playground.py
@@ -1,1 +1,14 @@
-__author__ = 'Andreas'
+class Calculator(object):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+    def add(self, a, b):
+        return a + b
+    def subtract(self, a, b):
+        return a - b
+    def divide(self, a, b):
+        return a / b
+    def multiply(self, a, b):
+        return a * b
+
+calculator = Calculator(1)
+print(calculator.multiply(3, 4))
