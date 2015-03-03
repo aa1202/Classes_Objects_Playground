c3e7f64a4cbe8729a3285065278ff41932c3bd88
Andreas Amundsen <andreas.amundsen123@gmail.com>
2015-03-03 14:45:32 +0100

    Calculator Script

--- Playground.py
+++ Playground.py
@@ -1,6 +1,4 @@
 class Calculator(object):
-    def __init__(self, arg1):
-        self.arg1 = arg1
     def add(self, a, b):
         return a + b
     def subtract(self, a, b):
@@ -10,5 +8,23 @@ class Calculator(object):
     def multiply(self, a, b):
         return a * b
 
-calculator = Calculator(1)
-print(calculator.multiply(3, 4))
+calculator = Calculator()
+while True:
+    operation = str(input("Do you want to add (A), subtract (S), divide (D), multiply (M) or quit (Q)?  ")).lower()
+    if operation != "q":
+        operator1 = int(input("Enter first number: "))
+        operator2 = int(input("Enter second number: "))
+    if operation == "a":
+        print(calculator.add(operator1, operator2))
+    elif operation == "s":
+        print(calculator.subtract(operator1, operator2))
+    elif operation == "d":
+        print(calculator.divide(operator1, operator2))
+    elif operation == "m":
+        print(calculator.subtract(operator1, operator2))
+    elif operation == "q":
+        quit()
+    else:
+        print("Not a valid input")
+
+
